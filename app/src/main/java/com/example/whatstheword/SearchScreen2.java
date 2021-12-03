e0d9eb336bc2aa102e037b98773a1d42d5bf51bd
Quincy Williams <QLWilliams@student.fullsail.edu>
2021-12-03 02:06:53 -0500

    Updated search screen 2
    Users who do not want to log in will see the keyboard automatically appear and they can search for words. Users also no longer have a lookup button as the action button on the keyboard will initiate the search and the keyboard will disappear

--- app/src/main/java/com/example/whatstheword/SearchScreen2.java
+++ app/src/main/java/com/example/whatstheword/SearchScreen2.java
@@ -1,12 +1,18 @@
 package com.example.whatstheword;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchScreen2 extends AppCompatActivity {
@@ -25,6 +31,22 @@ public class SearchScreen2 extends AppCompatActivity {
 
         defBox = (TextView) findViewById(R.id.definitionBox2);
         enterWord = (EditText) findViewById(R.id.search_bar2);
+        enterWord.requestFocus();
+
+        enterWord.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if(i == EditorInfo.IME_ACTION_DONE) {
+                    requestApi(textView);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(textView.getWindowToken(),0);
+                    enterWord.clearFocus();
+                    return true;
+                }
+
+                return false;
+            }
+        });
     }
 
     public void requestApi(View v)
@@ -43,9 +65,22 @@ public class SearchScreen2 extends AppCompatActivity {
         return "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id + "?" + "fields=" + fields + "&strictMatch=" + strictMatch;
     }
 
-    private void exit()
-    {
+    @Override
+    public void onBackPressed() {
         Intent intent = new Intent(SearchScreen2.this,MainActivity.class);
         startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home) {
+            Intent intent = new Intent(SearchScreen2.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
