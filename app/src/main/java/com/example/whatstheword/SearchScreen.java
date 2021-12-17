67332e715ffe1d794d4788614301a2e014ed4047
Quincy Williams <QLWilliams@student.fullsail.edu>
2021-12-16 22:40:42 -0500

    Updated Search Screen
    Added api to main search screen and added a clear button functionality. Adjusted keyboard to perform search

--- app/src/main/java/com/example/whatstheword/SearchScreen.java
+++ app/src/main/java/com/example/whatstheword/SearchScreen.java
@@ -3,10 +3,13 @@ package com.example.whatstheword;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,7 +22,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class SearchScreen extends AppCompatActivity {
 
-    private Button logoutButton;
+    private Button clearButton;
     private String url;
     public TextView defBox;
     public EditText enterWord;
@@ -31,6 +34,34 @@ public class SearchScreen extends AppCompatActivity {
 
         defBox = (TextView) findViewById(R.id.definitionBox);
         enterWord = (EditText) findViewById(R.id.search_bar);
+        clearButton = findViewById(R.id.search1ClearButton);
+
+        enterWord.requestFocus();
+        enterWord.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if(i == EditorInfo.IME_ACTION_DONE) {
+                    requestApi(textView);
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(textView.getWindowToken(),0);
+                    enterWord.clearFocus();
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enterWord.setText("");
+                enterWord.requestFocus();
+                defBox.setText("");
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(enterWord,InputMethodManager.SHOW_IMPLICIT);
+            }
+        });
     }
 
     public void requestApi(View v)
